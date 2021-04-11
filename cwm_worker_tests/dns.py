ebf7855cb8d0eb0a8b56b307c146f76b8f2831ec
Ori Hoch <ori@uumpa.com>
2021-04-11 23:23:57 +0300

    return nodes in dns check_domain

--- cwm_worker_tests/dns.py
+++ cwm_worker_tests/dns.py
@@ -17,5 +17,6 @@ def check_domain():
             nodes_missing_in_dns_ips.append({'ip': node_ip, 'name': node_name})
     return {
         'dns_ips_missing_in_nodes': dns_ips_missing_in_nodes,
-        'nodes_missing_in_dns_ips': nodes_missing_in_dns_ips
+        'nodes_missing_in_dns_ips': nodes_missing_in_dns_ips,
+        'nodes': nodes
     }
