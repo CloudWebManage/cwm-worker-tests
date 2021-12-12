aec1735633238ace97093c05ac49c606d074d9b6
Azeem Sajid <azeem.sajid@gmail.com>
2021-12-12 22:17:09 +0500

    Add download threads.

--- cwm_worker_tests/upload_download_test.py
+++ cwm_worker_tests/upload_download_test.py
@@ -1,15 +1,10 @@
 import os
 import subprocess
 import datetime
+from threading import Thread
 from minio import Minio
 from minio.error import S3Error
 
-
-def get_minio_client(endpoint, access_key, secret_key):
-    try:
-        return Minio(endpoint, access_key=access_key, secret_key=secret_key)
-    except S3Error as e:
-        print(f'❌ MinIO Exception: {e}')
 
 def get_now_string():
     return datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
@@ -32,12 +27,13 @@ def get_upload_filename():
     return 'cwm-test-' + get_now_string() + '.blob'
 
 
-def upload(client, bucket, num_files, file_size, output_dir):
-    print(f'📤 Upload test [bucket: {bucket}, num_files: {num_files}, file_size: {file_size}]')
+def upload(endpoint, access_key, secret_key, bucket, num_files, file_size, output_dir):
+    print(f'📤 Upload test')
 
     filepath = None
     try:
-        print(f'Checking bucket... [{bucket}]')
+        client = Minio(endpoint, access_key=access_key, secret_key=secret_key)
+
         bucket_exists = client.bucket_exists(bucket)
         if not bucket_exists:
             print(f'Bucket does not exist! [{bucket}]')
@@ -45,6 +41,8 @@ def upload(client, bucket, num_files, file_size, output_dir):
             print(f'Bucket created! [{bucket}]')
         else:
             print(f'Bucket exists! [{bucket}]')
+
+        start_time = datetime.datetime.now()
 
         # Create test file, remove at the end
         filepath = create_test_file(file_size)
@@ -55,44 +53,107 @@ def upload(client, bucket, num_files, file_size, output_dir):
             client.fput_object(bucket, filename, filepath)
             print(f'\rUploaded: {i+1} files', end='', flush=True)
 
-        print(f'\nFiles uploaded successfully!')
+        end_time = datetime.datetime.now()
+        elapsed_seconds = (end_time - start_time).total_seconds()
+        print(f'\nFiles uploaded! [{elapsed_seconds} seconds]')
 
     except S3Error as e:
-        print(f'❌ MinIO exception: {e}')
+        print(f'\nMinIO exception: {e}')
     except Exception as e:
-        print(f'❌ Exception: {e}')
+        print(f'\nException: {e}')
     finally:
         if filepath:
             os.remove(filepath)
 
 
-def download(client, bucket, download_iterations, download_threads, output_dir):
-    print(f'📥 Download test [bucket: {bucket}, download_iterations: {download_iterations}, download_threads: {download_threads}]')
+class DownloadIteration(Thread):
+
+    def __init__(self, threadid, endpoint, access_key, secret_key, bucket, download_iterations):
+        super().__init__()
+        self.threadid = threadid
+        self.endpoint = endpoint
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.bucket = bucket
+        self.download_iterations = download_iterations
+        self.stats = {}
+
+    def get_stats(self):
+        return self.stats
+
+    def run(self):
+        try:
+            thread_start_time = datetime.datetime.now()
+ 
+            context = f'Thread # {self.threadid}'
+            print(f'{context} | Started')
+
+            client = Minio(endpoint=self.endpoint, access_key=self.access_key, secret_key=self.secret_key)
+
+            bucket_exists = client.bucket_exists(self.bucket)
+            if not bucket_exists:
+                print(f'Bucket does not exist! [{self.bucket}]')
+                return
+
+            iterations = []
+            for iteration in range(self.download_iterations):
+                iteration_no = iteration + 1
+                iteration_start_time = datetime.datetime.now()
+                print(f'{context} | Iteration # {iteration_no} | Started')
+                objects = client.list_objects(self.bucket, recursive=True)
+                num_downloaded_files = 0
+                for obj in objects:
+                    object_name = obj.object_name
+                    object_filepath = f'/tmp/{object_name}.{self.threadid}' 
+                    client.fget_object(self.bucket, object_name, object_filepath)
+                    num_downloaded_files += 1
+                    os.remove(object_filepath)
+                # print(f'{context} | Downloaded: {num_downloaded_files} files', end='', flush=True)
+                iteration_end_time = datetime.datetime.now()
+                iteration_elapsed_seconds = (iteration_end_time - iteration_start_time).total_seconds()
+                print(f'{context} | Iteration # {iteration_no} | Finished [{iteration_elapsed_seconds} seconds] [{num_downloaded_files} files]')
+
+                iterations.append({
+                    'iteration': iteration_no,
+                    'elapsed_seconds': iteration_elapsed_seconds,
+                    'num_downloaded_files': num_downloaded_files
+                })
+
+            thread_end_time = datetime.datetime.now()
+            thread_elapsed_seconds = (thread_end_time - thread_start_time).total_seconds()
+            print(f'{context} | Finished [{thread_elapsed_seconds} seconds]')
+
+            self.stats = {
+                'threadid': self.threadid,
+                'thread_elapsed_seconds': thread_elapsed_seconds,
+                'iterations': iterations
+            }
+
+        except S3Error as e:
+            print(f'MinIO Exception: {e}')
+        except Exception as e:
+            print(f'Exception: {e}')
+
+
+def download(endpoint, access_key, secret_key, bucket, download_iterations, download_threads, output_dir):
+    print(f'📥 Download test')
 
     try:
-        print(f'Checking bucket... [{bucket}]')
-        bucket_exists = client.bucket_exists(bucket)
-        if not bucket_exists:
-            print(f'Bucket does not exist! [{bucket}]')
-            return
+        print(f'Spawning threads... [download_threads: {download_threads}]')
+        threads = []
+        for i in range(download_threads):
+            thread = DownloadIteration(i+1, endpoint=endpoint, access_key=access_key, secret_key=secret_key, bucket=bucket, download_iterations=download_iterations)
+            threads.append(thread)
+            thread.start()
 
-        print(f'Downloading files...')
-        objects = client.list_objects(bucket, recursive=True)
-        num_downloaded = 0
-        for obj in objects:
-            object_name = obj.object_name
-            object_filepath = '/tmp/' + object_name
-            client.fget_object(bucket, object_name, object_filepath)
-            num_downloaded += 1
-            print(f'\rDownloaded: {num_downloaded} files', end='', flush=True)
-            os.remove(object_filepath)
+        for thread in threads:
+            thread.join()
 
-        print(f'\nFiles downloaded successfully!')
+        for thread in threads:
+            print(thread.get_stats())
 
-    except S3Error as e:
-        print(f'❌ MinIO exception: {e}')
     except Exception as e:
-        print(f'❌ Exception: {e}')
+        print(f'Exception: {e}')
 
 
 def main(endpoint:str, access_key=str, secret_key=str, bucket=str, num_files=int, file_size=int,
@@ -102,12 +163,10 @@ def main(endpoint:str, access_key=str, secret_key=str, bucket=str, num_files=int
         print(f'  {k: >{20}}  =  {v}')
     print()
 
-    client = get_minio_client(endpoint=endpoint, access_key=access_key, secret_key=secret_key)
-
     if not only_download:
-        upload(client=client, bucket=bucket, num_files=num_files, file_size=file_size, output_dir=output_dir)
+        upload(endpoint=endpoint, access_key=access_key, secret_key=secret_key, bucket=bucket, num_files=num_files, file_size=file_size, output_dir=output_dir)
 
     if not only_upload:
-        download(client=client, bucket=bucket, download_iterations=download_iterations, download_threads=download_threads, output_dir=output_dir)
+        download(endpoint=endpoint, access_key=access_key, secret_key=secret_key, bucket=bucket, download_iterations=download_iterations, download_threads=download_threads, output_dir=output_dir)
 
     print(f'--- [DONE] ---')
